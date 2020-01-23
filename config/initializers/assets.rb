ae5d661b8c4b1bdecf1c765f80c43c43a5c61281
Braullio Vinnicius <braulliovg@gmail.com>
2020-01-23 01:41:11 -0300

    feat: update.conf.assets add js precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,7 +9,9 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
-Rails.application.config.assets.precompile += %w[drag_and_drop_lists.js]
+Rails.application.config.assets.precompile += %w[drag_and_drop_lists.js
+                                                 new_list.js
+                                                 modal_list_autocomplete.js]
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
