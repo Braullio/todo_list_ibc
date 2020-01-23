6cefab99525daeefd30e58da425fc6331ecf4946
Braullio Vinnicius <braulliovg@gmail.com>
2020-01-23 15:17:14 -0300

    refactor: add routes cards

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   root 'boards#index'
   resources :boards, only: %i[index show create update destroy]
   resources :lists, only: %i[create update destroy]
+  post 'cards/update_list', to: 'cards#update_list'
+  resources :cards, only: %i[create update destroy]
 end
