b431005636db99eb7175256cd788f11c6cfe933f
Braullio Vinnicius <braulliovg@gmail.com>
2020-01-23 15:19:12 -0300

    refactor: update.modal.card add has many cards

--- app/models/list.rb
+++ app/models/list.rb
@@ -6,9 +6,9 @@
 #
 #  id             :BIGINT(20)     not null, primary key
 #  tite           :VARCHAR(255)   not null
-#  description    :TEXT
 class List < ApplicationRecord
   belongs_to :board
+  has_many :cards, dependent: :destroy
 
   validates :title, presence: true
 end
