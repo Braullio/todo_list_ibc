1ebac9d12cdce7446767f95eefddfa3d8e8ce044
Braullio Vinnicius <braulliovg@gmail.com>
2020-01-23 15:14:07 -0300

    refactor: update.js.drag_and_drop add send to controller

--- app/assets/javascripts/drag_and_drop_lists.js
+++ app/assets/javascripts/drag_and_drop_lists.js
@@ -11,10 +11,38 @@ function drop(e) {
   var $data = e.dataTransfer.getData("element");
   var $code_html = document.getElementById($data);
   var $lister = document.getElementById(e.target.id);
+  var $count = $lister.getElementsByTagName('li').length;
+  
+  if (e.target.id.includes("li")) {
+    $card = $lister.parentElement.appendChild($code_html);
+    
+    $count = $lister.parentElement.getElementsByTagName('li').length;
+    $count -= 1;
+    $card.id = ($lister.parentElement.id + "li" + $count);
 
-  if (e.target.id.includes("ul")) {
-    e.target.appendChild($code_html);
+    var $list_id = $lister.parentElement.getAttribute("list");
+    var $card_id = $card.getAttribute("card");
+
   } else {
-    $lister.parentElement.appendChild($code_html);
+    $card = e.target.appendChild($code_html);
+
+    $count = $code_html.getElementsByTagName('li').length;
+    $card.id = ($lister.id + "li" + $count);
+
+    var $list_id = $lister.getAttribute("list");
+    var $card_id = $card.getAttribute("card");
+
   }
+
+  $.ajaxSetup({
+    headers: {
+      'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
+    }
+  });
+  
+  $.ajax({
+    url: "/cards/update_list", type: "post",
+    data: { update_list: JSON.stringify({ list_id: $list_id, card_id: $card_id }) }
+  }); 
+
 }
