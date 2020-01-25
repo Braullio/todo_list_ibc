bbeb87b01dad9e4c3c3da0aeccbfd6498b311e24
Braullio Vinnicius <braulliovg@gmail.com>
2020-01-25 18:38:44 -0300

    feat.routes add pages errors

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,9 @@ Rails.application.routes.draw do
   resources :lists, only: %i[create update destroy]
   post 'cards/update_list', to: 'cards#update_list'
   resources :cards, only: %i[create update destroy]
+
+  # Errors pages
+  get '/404', to: 'errors#not_found'
+  get '/422', to: 'errors#unacceptable'
+  get '/500', to: 'errors#internal_error'
 end
