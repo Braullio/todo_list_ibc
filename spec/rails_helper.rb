8ac8f658f0a5334bcfda4400950786dc101555bc
Braullio Vinnicius <braulliovg@gmail.com>
2020-01-26 00:48:45 -0300

    feat.spec add require 'support/factory_bot'

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,7 @@ require File.expand_path('../../config/environment', __FILE__)
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 require 'capybara/rspec'
+require 'support/factory_bot'
 
 require 'database_cleaner'
 DatabaseCleaner.strategy = :truncation
